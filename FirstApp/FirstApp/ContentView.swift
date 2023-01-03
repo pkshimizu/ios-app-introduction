c9afed4c3eadd474397a446d004ccb63f552205a
shimizu kenji <shimizu.kenji@noncore.net>
2023-01-03 18:20:23 +0900

    SwiftUI対応 たった二日でマスターできるiPhoneアプリ開発集中講座 Lesson2

---
+++ FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by 清水健司 on 2023/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var outputText = "Hello, World!"
+    
+    var body: some View {
+        VStack {
+            Text(outputText)
+                .font(.largeTitle)
+
+            Button("切り替えボタン") {
+                outputText = "Hello, Swift!"
+            }
+            .padding(.all)
+            .background(Color.blue)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 清水健司 on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
